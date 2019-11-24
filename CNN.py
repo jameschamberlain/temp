4b9bdb573414d35747cff37e8a1e9c145b480874
Sam <barrett370@gmail.com>
2019-11-24 22:41:29 +0000

    not working, tensor shape and kernel size issues

--- CNN.py
+++ CNN.py
@@ -10,8 +10,10 @@ import matplotlib.pyplot as plt
 from fastMRI.functions import transforms as T
 from typing import List
 
-# Load Data
+print(torch.cuda.get_device_properties(0))
+device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
 
+# Load Data
 data_path_train = '/home/sam/datasets/FastMRI/NC2019MRI/train'
 data_path_val = '/home/sam/datasets/FastMRI/NC2019MRI/train'
 data_list = load_data_path(data_path_train, data_path_val)
@@ -37,34 +39,36 @@ class ConvNet(torch.nn.Module):
     def __init__(self) -> None:
         super(ConvNet, self).__init__()
 
-        c = 640
-        c_1 = c
-        f_1 = 1
-        n_1 = 1  # number of convs to apply
+        c_0 = 640  # input channels
+        f_1 = 3  # spacial size of kernel
+        c_1 = 640  # output channels
+        n_1 = 10  # number of convs to apply
         l1_modules: List[nn.Module] = []
-        l1_conv_layers = [nn.Conv2d(c, c_1, kernel_size=f_1) for _ in range(n_1)]
-
-        l1_modules.append(*l1_conv_layers)
+        l1_conv_layers = [nn.Conv2d(in_channels=c_0, out_channels=c_1, kernel_size=f_1, stride=1, padding=1) for _ in
+                          range(n_1)]
+        l1_modules.extend(l1_conv_layers)
         l1_modules.append(nn.ReLU())
 
         self.hidden1 = nn.Sequential(*l1_modules)
 
-        n_2 = 1
-        c_2 = c_1
-        f_2 = 1
+        n_2 = 10  # number of convs to apply
+        c_2 = 640  # output channels
+        f_2 = 3  # size of kernel
         l2_modules = []
-        l2_conv_layers = [nn.Conv2d(c_1, c_2, kernel_size=f_2) for _ in range(n_2)]
-        l2_modules.append(*l2_conv_layers)
+        l2_conv_layers = [nn.Conv2d(in_channels=c_1, out_channels=c_2, kernel_size=f_2, stride=1, padding=1) for _ in
+                          range(n_2)]
+        l2_modules.extend(l2_conv_layers)
         l2_modules.append(nn.ReLU())
 
         self.hidden2 = nn.Sequential(*l2_modules)
 
         l3_modules = []
-        c_3 = c
-        n_3 = 1
-        f_3 = 1
-        l3_conv_layers = [nn.Conv2d(c_2, c_3, kernel_size=f_3) for _ in range(n_3)]
-        l3_modules.append(*l3_conv_layers)
+        c_3 = 640  # output channels = original input
+        n_3 = 1  # number of convolutions to apply
+        f_3 = 3  # size of kernel
+        l3_conv_layers = [nn.Conv2d(in_channels=c_2, out_channels=c_3, kernel_size=f_3, stride=1, padding=1) for _ in
+                          range(n_3)]
+        l3_modules.extend(l3_conv_layers)
 
         self.hidden3 = nn.Sequential(*l3_modules)
 
@@ -76,31 +80,49 @@ class ConvNet(torch.nn.Module):
         return out
 
 
-model = ConvNet()
+RED = '\033[0;31m'
+GREEN = '\033[0;32m'
+YELLOW = '\033[0;33m'
+from sys import stdout
 
-criterion = nn.MSELoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=EPSILON)
-total_step = len(train_loader)
-n_epochs = 5
-loss_list = list()
-acc_list = list()
-for epoch in range(n_epochs):
-    i = 0
-    for iteration, sample in enumerate(train_loader):
-        img_gt, img_und, rawdata_und, masks, norm = sample
-        output = model(img_und)
-        loss = criterion(output, img_gt)
-        loss_list.append(loss.item())
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        total = img_gt.size(0)
-        _, predicted = torch.max(output.data, 1)
-        correct = (predicted == img_gt).sum().item()
-        acc_list.append(correct / total)
+printc = lambda x: stdout.write(x)
+if __name__ == "__main__":
+    printc(YELLOW)
+    printc(device)
+    model = ConvNet().to(device)
+    printc(GREEN)
+    printc("constructed model\n")
+    criterion = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=EPSILON)
+    total_step = len(train_loader)
+    n_epochs = 5
+    loss_list = list()
+    acc_list = list()
+    print("Starting training")
+    for epoch in range(n_epochs):
+        i = 0
+        for iteration, sample in enumerate(train_loader):
+            img_gt, img_und, rawdata_und, masks, norm = sample
+            printc(YELLOW)
+            print(img_und.size())
+            print(img_gt.size())
+            # output = model(img_und.to(device))
+            input = T.complex_abs(img_und).squeeze()
+            input = input.unsqueeze(-1).unsqueeze(-1).transpose_(0, 1).to(device)
+            print(input.shape)
+            output = model(input)
+            loss = criterion(output, T.complex_abs(img_gt.squeeze().to(device)))
+            loss_list.append(loss.item())
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            total = img_gt.size(0)
+            _, predicted = torch.max(output.data, 1)
+            correct = (predicted == img_gt).sum().item()
+            acc_list.append(correct / total)
 
-        if (i + 1) % 100 == 0:
-            print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Accuracy: {:.2f}%'
-                  .format(epoch + 1, n_epochs, i + 1, total_step, loss.item(),
-                          (correct / total) * 100))
-        i += 1
+            if (i + 1) % 100 == 0:
+                print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Accuracy: {:.2f}%'
+                      .format(epoch + 1, n_epochs, i + 1, total_step, loss.item(),
+                              (correct / total) * 100))
+            i += 1
