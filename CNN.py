af4db8c357a983dfc8b8bf3a16a3ebfaea94d926
Sam <barrett370@gmail.com>
2019-11-24 16:32:39 +0000

    added CNN.l1

--- CNN.py
+++ CNN.py
@@ -35,32 +35,27 @@ class ConvNet(torch.nn.Module):
 
     def __init__(self) -> None:
         super(ConvNet, self).__init__()
-        n_features = 0
-        n_output = 1
 
-        self.hidden0 = nn.Sequential(
-            nn.Conv2d(640, 320, kernel_size=5, stride=1, padding=2),
-            nn.LeakyReLU(0.2),
-            nn.MaxPool2d(kernel_size=2, stride=2)
-        )
+        c = 0
+        c_out = c
+        f_1 = 0
+        n_1 = 1  # number of convs to apply
+
+        conv_layers = [nn.Conv2d(c, c_out, kernel_size=f_1, stride=1, padding=2) for i in range(n_1)]
+
+        l1_modules = conv_layers.append(nn.ReLU)
+
+        self.hidden1 = nn.Sequential(*l1_modules)
 
         self.hidden1 = nn.Sequential(
-            nn.Conv2d(320, 640, kernel_size=5, stride=1, padding=2),
+            nn.Conv2d(640, 4 * 640, kernel_size=5, stride=1, padding=2),
             nn.LeakyReLU(0.2),
-            nn.MaxPool2d(kernel_size=2, stride=2)
+            # nn.MaxPool2d(kernel_size=2, stride=2)
         )
 
-        self.drop_out = nn.Dropout()
-        self.fc1 = nn.Linear(7 * 7 * 64, 1000)
-        self.fc2 = nn.Linear(1000, 10)
-
     def forward(self, x):
-        out = self.hidden0(x)
-        out = self.hidden1(out)
+        out = self.hidden1(x)
         out = out.reshape(out.size(0), -1)
-        out = self.drop_out(out)
-        out = self.fc1(out)
-        out = self.fc2(out)
         return out
 
 
@@ -76,7 +71,6 @@ i = 0
 for epoch in range(n_epochs):
     for iteration, sample in enumerate(train_loader):
         img_gt, img_und, rawdata_und, masks, norm = sample
-        print(np.shape(img_und))
         output = model(img_und)
         loss = criterion(output, img_gt)
         loss_list.append(loss.item())
@@ -92,4 +86,4 @@ for epoch in range(n_epochs):
             print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Accuracy: {:.2f}%'
                   .format(epoch + 1, n_epochs, i + 1, total_step, loss.item(),
                           (correct / total) * 100))
-        i+=1
+        i += 1
