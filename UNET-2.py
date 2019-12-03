5a3133a443a0310d04a6d356b322cf7434c53293
Bianca Bunaciu <bab581@cs.bham.ac.uk>
2019-12-03 13:35:21 +0000

    Using ray framework for hyperparameter tune

--- UNET-2.py
+++ UNET-2.py
@@ -68,118 +68,151 @@ class UNet(nn.Module):
 
         return self.conv2(y)
 
-
 device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
 
-# Load Data
-print("Data loading...")
-data_path_train = '/data/local/NC2019MRI/train'
-data_list = load_data_path(data_path_train, data_path_train)
+ray.init()
 
-# Split dataset into train-validate
-validation_split = 0.1
-dataset_len = len(data_list['train'])
-indices = list(range(dataset_len))
+# The number of sets of random hyperparameters to try.
+num_evaluations = 10
 
-# Randomly splitting indices:
-val_len = int(np.floor(validation_split * dataset_len))
-validation_idx = np.random.choice(indices, size=val_len, replace=False)
-train_idx = list(set(indices) - set(validation_idx))
 
-data_list_train = [data_list['train'][i] for i in train_idx]
-data_list_val = [data_list['val'][i] for i in validation_idx]
-
+# A function for generating random hyperparameters.
+def generate_hyperparameters():
+    return {
+        "learning_rate": 10**np.random.uniform(-5, 1),
+        "batch_size": np.random.randint(1, 100),
+    }
 
 acc = hyper_param.N_FOLD
 cen_fract = 0.04
 seed = False  # random masks for each slice
 num_workers = 8
 
-# create data loader for training set. It applies same to validation set as well
-train_dataset = MRIDataset(data_list_train, acceleration=acc, center_fraction=cen_fract, use_seed=seed)
-train_loader = DataLoader(train_dataset, shuffle=True, batch_size=hyper_param.BATCH_SIZE, num_workers=num_workers)
+def get_data_loaders(batch_size):
+    print("Data loading...")
+    data_path_train = '/data/local/NC2019MRI/train'
+    data_list = load_data_path(data_path_train, data_path_train)
 
-val_dataset = MRIDataset(data_list_val, acceleration=acc, center_fraction=cen_fract, use_seed=seed)
-val_loader = DataLoader(val_dataset, shuffle=True, batch_size=1, num_workers=num_workers)
+    # Split dataset into train-validate
+    validation_split = 0.1
+    dataset_len = len(data_list['train'])
+    indices = list(range(dataset_len))
 
-data_loaders = {"train": train_loader, "val": val_loader}
-data_lengths = {"train": len(train_idx), "val": val_len}
-print("Data loaded")
+    # Randomly splitting indices
+    val_len = int(np.floor(validation_split * dataset_len))
+    validation_idx = np.random.choice(indices, size=val_len, replace=False)
+    train_idx = list(set(indices) - set(validation_idx))
+
+    data_list_train = [data_list['train'][i] for i in train_idx]
+    data_list_val = [data_list['val'][i] for i in validation_idx]
+
+    # Create data loader for training set. It applies same to validation set as well
+    train_dataset = MRIDataset(data_list_train, acceleration=acc, center_fraction=cen_fract, use_seed=seed)
+    train_loader = DataLoader(train_dataset, shuffle=True, batch_size=batch_size, num_workers=num_workers)
+
+    val_dataset = MRIDataset(data_list_val, acceleration=acc, center_fraction=cen_fract, use_seed=seed)
+    val_loader = DataLoader(val_dataset, shuffle=True, batch_size=batch_size, num_workers=num_workers)
+    print("Data loaded")
+
+    return train_loader, val_loader
+
+def train(model, optimiser, criterion, train_loader):
+    print("Starting training")
+    model.train()
+    
+    for i, sample in enumerate(train_loader):
+            img_gt, img_und, _, _, _ = sample
+            img_in = T.center_crop(T.complex_abs(img_und).unsqueeze(0), [320, 320]).to(device)
+
+            output = model(img_in)
+            optimiser.zero_grad()
+
+            loss = - criterion(output, T.center_crop(T.complex_abs(img_gt).unsqueeze(0), [320, 320]).to(device))
+            loss.backward()
+            optimiser.step()
+
+def test(model, test_loader):
+    model.eval()
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for i, sample in enumerate(test_loader):
+            img_gt, img_und, _, _, _ = sample
+            img_in = T.center_crop(T.complex_abs(img_und).unsqueeze(0), [320, 320]).to(device)
+
+            output = model(img_in)
+
+            real = T.center_crop(T.complex_abs(img_gt), [320, 320]).squeeze()
+
+            # TODO evaluate model
+            #_, predicted = torch.max(outputs.data, 1)
+            #total += target.size(0)
+            #correct += (predicted == target).sum().item()
+
+    return correct / total
+
+@ray.remote
+def evaluate_hyperparameters(config):
+    print("Constructed model")
+    model = UNet(1, 1, hyper_param.CHANNELS).to(device)
+    train_loader, val_loader = get_data_loaders(config["batch_size"])
+
+    criterion = pytorch_ssim.SSIM()
+    # criterion = nn.MSELoss()
+    optimiser = optim.Adam(model.parameters(), lr=config["learning_rate"])
+    #optimizer = optim.SGD(
+    #    model.parameters(),
+    #    lr=config["learning_rate"],
+    #    momentum=config["momentum"])
+    train(model, optimiser, criterion, train_loader)
+    return test(model, val_loader)
 
 
 if __name__ == "__main__":
     print(device)
-    model = UNet(1, 1, hyper_param.CHANNELS).to(device)
-    print("Constructed model")
-    # criterion = nn.MSELoss()
-    criterion = pytorch_ssim.SSIM()
-    #optimiser = optim.SGD(model.parameters(), lr=EPSILON)
-    optimiser = optim.Adam(model.parameters(), lr=hyper_param.EPSILON)
 
-    total_step = len(train_loader)
-    n_epochs = hyper_param.N_EPOCHS
-    batch_loss = []
-    train_loss = []
-    val_loss = []
+    # Keep track of the best hyperparameters and the best accuracy.
+    best_hyperparameters = None
+    best_accuracy = 0
+    # A list holding the object IDs for all of the experiments that we have
+    # launched but have not yet been processed.
+    remaining_ids = []
+    # A dictionary mapping an experiment's object ID to its hyperparameters.
+    # hyerparameters used for that experiment.
+    hyperparameters_mapping = {}
 
-    print("Starting training")
-    model.train()
-    for epoch in range(n_epochs):
-        print('Epoch {}/{}'.format(epoch, n_epochs - 1))
-        print('-' * 10)
+    # Randomly generate sets of hyperparameters and launch a task to evaluate it.
+    for i in range(num_evaluations):
+        hyperparameters = generate_hyperparameters()
+        accuracy_id = evaluate_hyperparameters.remote(hyperparameters)
+        remaining_ids.append(accuracy_id)
+        hyperparameters_mapping[accuracy_id] = hyperparameters
 
-        if(epoch == 40):
-            # Reduce learning rate
-            for g in optimiser.param_groups:
-                g['lr'] = hyper_param.EPSILON * 0.1
+    # Fetch and print the results of the tasks in the order that they complete.
+    while remaining_ids:
+        # Use ray.wait to get the object ID of the first task that completes.
+        done_ids, remaining_ids = ray.wait(remaining_ids)
+        # There is only one return result by default.
+        result_id = done_ids[0]
 
-        # Each epoch has a training and validation phase
-        for phase in ['train', 'val']:
-            if phase == 'train':
-                model.train(True)  # Set model to training mode
-            else:
-                model.train(False)  # Set model to evaluate mode
+        hyperparameters = hyperparameters_mapping[result_id]
+        accuracy = ray.get(result_id)
+        print("""We achieve accuracy {:.3}% with
+            learning_rate: {:.2}
+            batch_size: {}
+            momentum: {:.2}
+        """.format(100 * accuracy, hyperparameters["learning_rate"],
+                    hyperparameters["batch_size"], hyperparameters["momentum"]))
+        if accuracy > best_accuracy:
+            best_hyperparameters = hyperparameters
+            best_accuracy = accuracy
 
-            running_loss = 0.0
-
-            # Iterate over data.
-            for i, sample in enumerate(data_loaders[phase]):
-                img_gt, img_und, rawdata_und, masks, norm = sample
-                img_in = T.center_crop(T.complex_abs(img_und).unsqueeze(0), [320, 320]).to(device)
-
-                output = model(img_in)
-                optimiser.zero_grad()
-
-                loss = - criterion(output, T.center_crop(T.complex_abs(img_gt).unsqueeze(0), [320, 320]).to(device))
-                
-                # backward + optimise only if in training phase
-                if phase == 'train':
-                    loss.backward()
-                    optimiser.step()
-
-                # calculate loss
-                batch_loss.append(- loss.item())
-                running_loss += - loss.item() * img_in.size(0) 
-
-            epoch_loss = running_loss / data_lengths[phase]
-            print('{} Loss: {:.4f}'.format(phase, epoch_loss))
-
-            if phase == 'train':
-                train_loss.append(epoch_loss)
-            else:
-                val_loss.append(epoch_loss)
-    
-    # Print stats
-    print("Minimum loss:", min(batch_loss))
-    print("Maximum loss:", max(batch_loss))
-    print("Average loss:", sum(batch_loss) / len(batch_loss))
-
-    print("Train loss: ", train_loss)
-    print("Val loss: ", val_loss)
-    fig = plt.figure()
-    plt.plot(range(n_epochs), train_loss)
-    plt.plot(range(n_epochs), val_loss)
-    plt.savefig(f'diagrams/UNET_2-{hyper_param.DESCR}.png')
-
-    # save model
-    torch.save(model.state_dict(), f"./models/UNET_2-{hyper_param.DESCR}")
+    # Record the best performing set of hyperparameters.
+    print("""Best accuracy over {} trials was {:.3} with
+        learning_rate: {:.2}
+        batch_size: {}
+        momentum: {:.2}
+        """.format(num_evaluations, 100 * best_accuracy,
+                    best_hyperparameters["learning_rate"],
+                    best_hyperparameters["batch_size"],
+                    best_hyperparameters["momentum"]))
