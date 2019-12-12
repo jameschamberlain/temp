07ce338ca22430ccc8b2959ec6046779f9a80385
Sam Barrett <barrett370@gmail.com>
2019-12-12 11:00:42 +0000

    pop stash

--- vary-optim/all_results_plotter.py
+++ vary-optim/all_results_plotter.py
@@ -1,27 +1,33 @@
 import pickle
 import matplotlib.pyplot as plt
 import pandas as pd
-optimisers = ['adagrad','adam','adamax','adamW','ASGD','SGD']
-
+# optimisers = ['adagrad','adam','adamax','adamW','ASGD','SGD']
+optimisers = ['adamW', 'adam']
 train_losses = []
 val_losses = []
-df=pd.DataFrame({'adagrad': {'train':[], 'val':[]}, 'adam': {'train':[], 'val':[]}, 'adamax': {'train':[], 'val':[]}, 'adamW': {'train':[], 'val':[]}, 'ASGD': {'train':[], 'val':[]},'SGD': {'train':[], 'val':[]}  })
+# df=pd.DataFrame({'adagrad': {'train':[], 'val':[]}, 'adam': {'train':[], 'val':[]}, 'adamax': {'train':[], 'val':[]}, 'adamW': {'train':[], 'val':[]}, 'ASGD': {'train':[], 'val':[]},'SGD': {'train':[], 'val':[]}  })
+df = pd.DataFrame({'adamW': {'train': [], 'val': []},
+                   'adam': {'train': [], 'val': []}})
 for optimiser in optimisers:
-    
-    df[optimiser]['train'] = pickle.load(open(f"./vary-optim/pickles/train_loss_{optimiser}.pkl",'rb'))
-    df[optimiser]['val'] = pickle.load(open(f"./vary-optim/pickles/val_loss_{optimiser}.pkl", 'rb'))
+
+    df[optimiser]['train'] = pickle.load(
+        open(f"./vary-optim/pickles/train_loss_{optimiser}.pkl", 'rb'))
+    df[optimiser]['val'] = pickle.load(
+        open(f"./vary-optim/pickles/val_loss_{optimiser}.pkl", 'rb'))
 
 # print(df)
 plt.xlabel('Epochs')
 plt.ylabel('loss, DSSIM')
-colours = ['b','g','r','c','m','y']
+colours = ['b', 'g', 'r', 'c', 'm', 'y']
 
 for optimiser in optimisers:
     for phase in df[optimiser].keys():
         if phase == 'val':
-            plt.plot(df[optimiser][phase], label=f"{optimiser} {phase}",linestyle='dashed',color=colours[list(df.keys()).index(optimiser)])
+            plt.plot(df[optimiser][phase], label=f"{optimiser} {phase}", linestyle='dashed', color=colours[list(
+                df.keys()).index(optimiser)])
         else:
-            plt.plot(df[optimiser][phase], label=f"{optimiser} {phase}",color=colours[list(df.keys()).index(optimiser)])
+            plt.plot(df[optimiser][phase], label=f"{optimiser} {phase}", color=colours[list(
+                df.keys()).index(optimiser)])
 # plt.ylim(0,max(TRAIN_LOSS))
 plt.title('Loss against epoch number for all optimisers')
 plt.legend()
